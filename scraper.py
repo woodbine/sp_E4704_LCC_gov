c0c7f17d07f0433920ea57fbd5aed8a586bb866c
woodbine <ian@spendnetwork.com>
2015-08-20 11:30:36 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -57,7 +57,7 @@ def convert_mth_strings ( mth_string ):
     return mth_string
 # pull down the content from the webpage
 html = urllib2.urlopen(url)
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, "lxml")
 # find all entries with the required class
 block = soup.find('ul', attrs = {'class':'unstyled resource-list'})
 links = block.find_all('div', 'btn-group-vertical')
